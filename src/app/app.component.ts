024a323ead7d49689208a782b395ee03f03ceaa7
kumaruidivloper <kumaruidivloper@gmail.com>
2019-04-28 18:30:29 +0530

    Mat-Tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular-Material-Tutorial';
 
-  public opened = false;
-
-  log(state) {
-    console.log(state);
+  logChange(index) {
+    console.log(index)
   }
 }
